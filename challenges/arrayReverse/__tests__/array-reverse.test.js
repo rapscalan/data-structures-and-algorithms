98e4986e811a04d5a57fd3c73a948bdf683eae09
Zak Hyslin <rapscalan@gmail.com>
2019-12-03 12:32:56 -0800

    travis spacing

--- challenges/arrayReverse/__tests__/array-reverse.test.js
+++ challenges/arrayReverse/__tests__/array-reverse.test.js
@@ -7,17 +7,17 @@ describe('array reverse', ()=> {
     expect(arrayReverse([1])).toEqual([1]);
     expect(arrayReverse([1, 1, 1, 2, 3, 1, 1, 1])).toEqual([1, 1, 1, 3, 2, 1, 1, 1]);
   });
-    it('reverses an array with foreach', () => {
-        expect(arrayReverse2([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
-        expect(arrayReverse2([])).toEqual([]);
-        expect(arrayReverse2([1])).toEqual([1]);
-        expect(arrayReverse2([1, 1, 1, 2, 3, 1, 1, 1])).toEqual([1, 1, 1, 3, 2, 1, 1, 1]);
-    });
-    it('reverses an array with unshift', () => {
-        expect(arrayReverse3([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
-        expect(arrayReverse3([])).toEqual([]);
-        expect(arrayReverse3([1])).toEqual([1]);
-        expect(arrayReverse3([1, 1, 1, 2, 3, 1, 1, 1])).toEqual([1, 1, 1, 3, 2, 1, 1, 1]);
-    });
+  it('reverses an array with foreach', () => {
+    expect(arrayReverse2([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
+    expect(arrayReverse2([])).toEqual([]);
+    expect(arrayReverse2([1])).toEqual([1]);
+    expect(arrayReverse2([1, 1, 1, 2, 3, 1, 1, 1])).toEqual([1, 1, 1, 3, 2, 1, 1, 1]);
+  });
+  it('reverses an array with unshift', () => {
+    expect(arrayReverse3([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
+    expect(arrayReverse3([])).toEqual([]);
+    expect(arrayReverse3([1])).toEqual([1]);
+    expect(arrayReverse3([1, 1, 1, 2, 3, 1, 1, 1])).toEqual([1, 1, 1, 3, 2, 1, 1, 1]);
+  });
     
 });
