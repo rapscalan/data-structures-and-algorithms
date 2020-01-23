20da81308d678505391685712a02637277861399
Zak Hyslin <rapscalan@gmail.com>
2020-01-22 19:28:30 -0800

    daylight savings

--- challenges/dailyRevenue/dailyRevenue.js
+++ challenges/dailyRevenue/dailyRevenue.js
@@ -1,6 +1,7 @@
 const revenue = (transactions) => {
+  const offset = (new Date().getTimezoneOffset() - 60) * 1000 * 60;
   const result = transactions.reduce((acc, el) => {
-    let date = new Date(el.timestamp).toDateString();
+    let date = new Date(el.timestamp + offset).toDateString();
     acc[date] = (acc[date] || 0) + el.price;
     return acc;
   }, {});
