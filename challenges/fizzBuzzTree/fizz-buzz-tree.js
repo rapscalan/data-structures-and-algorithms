bd907530e0239e42e761d9e31226f6bfc5e64d0e
Zak Hyslin <rapscalan@gmail.com>
2020-01-30 02:30:54 -0800

    export

--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -10,10 +10,12 @@ const fizzBuzzTree = (aTree)=>{
   let curr = aTree;
   if(curr.value % 3 === 0 && curr.value % 5 === 0){
     //create node with fizzbuzz
-  }else if(curr.value % 3 === 0){
+  } else if(curr.value % 3 === 0){
     //create node with fizz
-  }else if(curr.value % 5 === 0){
+  } else if(curr.value % 5 === 0){
     //create node with buzz
   }
 
-}
+  module.exports = fizzBuzzTree;
+
+};
