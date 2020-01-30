3a61a10c082c78f5159044edef8a63b3d4a7467f
Zak Hyslin <rapscalan@gmail.com>
2020-01-30 02:29:29 -0800

    logic for node

--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -7,6 +7,13 @@ class Node{
 }
 
 const fizzBuzzTree = (aTree)=>{
-  
+  let curr = aTree;
+  if(curr.value % 3 === 0 && curr.value % 5 === 0){
+    //create node with fizzbuzz
+  }else if(curr.value % 3 === 0){
+    //create node with fizz
+  }else if(curr.value % 5 === 0){
+    //create node with buzz
+  }
 
 }
